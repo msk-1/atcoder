540b5f4c13e67afba169a6391be07ec441ecb898
hdy msk <h69.masaki@gmail.com>
2019-09-06 23:53:59 +0900

    b 5min

--- b_resistorsInParallel.py
+++ b_resistorsInParallel.py
@@ -1,1 +1,16 @@
 # https://atcoder.jp/contests/abc138/tasks/abc138_b
+
+import sys
+lst = []
+for line in sys.stdin:
+    if line == '\n':
+        break
+    else :
+        lst.append(line)
+
+n = int(lst[0])
+intList = lst[1].split(' ')
+mother = 0
+for item in intList:
+    mother = mother + (1/int(item))
+print(1/mother)
